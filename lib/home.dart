b68c37558a60b8f5a0eff15cfc448aa2664d8498
neybord <ryan.gaeta.15@gmail.com>
2022-05-18 22:31:56 -0300

    Layout em 50% (Eu acho)

--- lib/home.dart
+++ lib/home.dart
@@ -19,7 +19,7 @@ class _HomeState extends State<Home> {
   }
 
   paginaLayout() {
-    double tamanhoFotosAltura = 80;
+    double tamanhoFotosAltura = 100;
     return Container(
       color: Color.fromRGBO(49, 218, 141, 1),
       width: double.infinity,
@@ -27,42 +27,104 @@ class _HomeState extends State<Home> {
         children: [
           Container(
             alignment: Alignment.center,
-            color: Colors.white,
+            color: Color.fromRGBO(49, 218, 141, 1),
             width: double.infinity,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 Container(
+                  margin: EdgeInsets.symmetric(vertical: 10),
                   decoration: BoxDecoration(
-                    color: Color(0xAAFF0000),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
+                      borderRadius: BorderRadius.circular(10),
+                      //border: Border.all(color: Colors.black,width: 1),
+                      image: DecorationImage(
+                          image: NetworkImage("Assets/Imagens/capuccino.png"),
+                          fit: BoxFit.cover),
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 4,
+                            spreadRadius: 0.1,
+                            offset: Offset(2, 4))
+                      ]),
                   alignment: Alignment.center,
                   width: 100,
                   height: tamanhoFotosAltura,
-                  child: Text("Container1"),
                 ),
                 Container(
                   decoration: BoxDecoration(
-                    color: Color(0xAAFF0000),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
+                      borderRadius: BorderRadius.circular(10),
+                      //border: Border.all(color: Colors.black,width: 1),
+                      image: DecorationImage(
+                          image: NetworkImage("Assets/Imagens/capuccino.png"),
+                          fit: BoxFit.cover),
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 4,
+                            spreadRadius: 0.1,
+                            offset: Offset(2, 4))
+                      ]),
                   alignment: Alignment.center,
                   width: 100,
                   height: tamanhoFotosAltura,
-                  child: Text("Container2"),
                 ),
                 Container(
                   decoration: BoxDecoration(
-                    color: Color(0xAAFF0000),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
+                      borderRadius: BorderRadius.circular(10),
+                      //border: Border.all(color: Colors.black,width: 1),
+                      image: DecorationImage(
+                          image: NetworkImage("Assets/Imagens/capuccino.png"),
+                          fit: BoxFit.cover),
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 4,
+                            spreadRadius: 0.1,
+                            offset: Offset(2, 4))
+                      ]),
                   alignment: Alignment.center,
                   width: 100,
                   height: tamanhoFotosAltura,
-                  child: Text("Container3"),
                 ),
               ],
+            ),
+          ),
+          Expanded(
+            flex: 10,
+            child: Container(
+              color: Color.fromRGBO(49, 218, 141, 1),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Column(
+                        children: [Text("Imagem")],
+                      ),
+                      Column(
+                        children: [
+                          Row(
+                            children: [Text("Titulo")],
+                          ),
+                          Row(
+                            children: [Text("Descrição")],
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                  Row(),
+                  Row(),
+                  Row(),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.green[900],
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [Text("data")],
+              ),
             ),
           )
         ],
